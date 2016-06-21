52d45c210478188dadcc0fff9adc8ed899a5830c
fagio-darkie <fagio.darkie@gmail.com>
2016-06-21 15:48:45 +0200

    Implemented NewOperation in ValInterpreter, like in Basic Interpreter.

--- src/com/laneve/asp/ASMAnalysis/asmClasses/ValInterpreter.java
+++ src/com/laneve/asp/ASMAnalysis/asmClasses/ValInterpreter.java
@@ -2,14 +2,20 @@ package com.laneve.asp.ASMAnalysis.asmClasses;
 
 import java.util.List;
 
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.objectweb.asm.tree.analysis.BasicValue;
 import org.objectweb.asm.tree.analysis.Interpreter;
 
 import com.laneve.asp.ASMAnalysis.asmTypes.AnValue;
 
-public class ValInterpreter extends Interpreter<AnValue>{
+public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
 
 	protected ValInterpreter(int api) {
 		super(api);
@@ -17,14 +23,72 @@ public class ValInterpreter extends Interpreter<AnValue>{
 
 	@Override
 	public AnValue newValue(Type type) {
-		// TODO Auto-generated method stub
-		return null;
+		return new AnValue(type);
 	}
 
 	@Override
 	public AnValue newOperation(AbstractInsnNode insn) throws AnalyzerException {
-		// TODO Auto-generated method stub
-		return null;
+		switch (insn.getOpcode()) {
+        case ACONST_NULL:
+            return newValue(Type.getObjectType("null"));
+        case ICONST_M1:
+        case ICONST_0:
+        case ICONST_1:
+        case ICONST_2:
+        case ICONST_3:
+        case ICONST_4:
+        case ICONST_5:
+            return newValue(Type.INT_TYPE);
+        case LCONST_0:
+        case LCONST_1:
+            return newValue(Type.LONG_TYPE);
+        case FCONST_0:
+        case FCONST_1:
+        case FCONST_2:
+            return newValue(Type.FLOAT_TYPE);
+        case DCONST_0:
+        case DCONST_1:
+            return newValue(Type.DOUBLE_TYPE);
+        case BIPUSH:
+        case SIPUSH:
+            return newValue(Type.INT_TYPE);
+        case LDC:
+            Object cst = ((LdcInsnNode) insn).cst;
+            if (cst instanceof Integer) {
+                return newValue(Type.INT_TYPE);
+            } else if (cst instanceof Float) {
+                return newValue(Type.FLOAT_TYPE);
+            } else if (cst instanceof Long) {
+                return newValue(Type.LONG_TYPE);
+            } else if (cst instanceof Double) {
+                return newValue(Type.DOUBLE_TYPE);
+            } else if (cst instanceof String) {
+                return newValue(Type.getObjectType("java/lang/String"));
+            } else if (cst instanceof Type) {
+                int sort = ((Type) cst).getSort();
+                if (sort == Type.OBJECT || sort == Type.ARRAY) {
+                    return newValue(Type.getObjectType("java/lang/Class"));
+                } else if (sort == Type.METHOD) {
+                    return newValue(Type
+                            .getObjectType("java/lang/invoke/MethodType"));
+                } else {
+                    throw new IllegalArgumentException("Illegal LDC constant "
+                            + cst);
+                }
+            } else if (cst instanceof Handle) {
+                return newValue(Type
+                        .getObjectType("java/lang/invoke/MethodHandle"));
+            } else {
+                throw new IllegalArgumentException("Illegal LDC constant "
+                        + cst);
+            }
+        case GETSTATIC:
+            return newValue(Type.getType(((FieldInsnNode) insn).desc));
+        case NEW:
+            return newValue(Type.getObjectType(((TypeInsnNode) insn).desc));
+        default:
+            throw new Error("Internal error.");
+        }
 	}
 
 	@Override
