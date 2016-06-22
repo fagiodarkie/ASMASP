eb9b97f5abd9a1c1c6f39698d03e7feff26d8b1a
fagio-darkie <fagio.darkie@gmail.com>
2016-06-22 16:43:42 +0200

    Removed copy of AnValue. Implemented some operators in ValInterpreter with extensions on AnValue class.

--- src/com/laneve/asp/ASMAnalysis/asmClasses/ValInterpreter.java
+++ src/com/laneve/asp/ASMAnalysis/asmClasses/ValInterpreter.java
@@ -14,6 +14,7 @@ import org.objectweb.asm.tree.analysis.BasicValue;
 import org.objectweb.asm.tree.analysis.Interpreter;
 
 import com.laneve.asp.ASMAnalysis.asmTypes.AnValue;
+import com.laneve.asp.ASMAnalysis.asmTypes.AnValue.ExpressionType;
 
 public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
 
@@ -28,6 +29,7 @@ public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
 
 	@Override
 	public AnValue newOperation(AbstractInsnNode insn) throws AnalyzerException {
+		AnValue v;
 		switch (insn.getOpcode()) {
         case ACONST_NULL:
             return newValue(Type.getObjectType("null"));
@@ -38,31 +40,50 @@ public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
         case ICONST_3:
         case ICONST_4:
         case ICONST_5:
-            return newValue(Type.INT_TYPE);
+        	v = newValue(Type.INT_TYPE);
+        	v.setExpressionValue(AnValue.getConstValue(insn.getOpcode()), ExpressionType.CONST);
+            return v;
         case LCONST_0:
         case LCONST_1:
-            return newValue(Type.LONG_TYPE);
+        	v = newValue(Type.LONG_TYPE);
+        	v.setExpressionValue(AnValue.getConstValue(insn.getOpcode()), ExpressionType.CONST);
+            return v;
         case FCONST_0:
         case FCONST_1:
         case FCONST_2:
-            return newValue(Type.FLOAT_TYPE);
+        	v = newValue(Type.FLOAT_TYPE);
+        	v.setExpressionValue(AnValue.getConstValue(insn.getOpcode()), ExpressionType.CONST);
+            return v;
         case DCONST_0:
         case DCONST_1:
-            return newValue(Type.DOUBLE_TYPE);
+        	v = newValue(Type.DOUBLE_TYPE);
+        	v.setExpressionValue(AnValue.getConstValue(insn.getOpcode()), ExpressionType.CONST);
+            return v;
         case BIPUSH:
         case SIPUSH:
-            return newValue(Type.INT_TYPE);
+        	v = newValue(Type.INT_TYPE);
+        	v.setExpressionValue("ic" + v.getID(), ExpressionType.CONST);
+            return v;
         case LDC:
             Object cst = ((LdcInsnNode) insn).cst;
             if (cst instanceof Integer) {
-                return newValue(Type.INT_TYPE);
+            	v = newValue(Type.INT_TYPE);
+            	v.setExpressionValue(((Integer) cst).toString(), ExpressionType.CONST);
+                return v;
             } else if (cst instanceof Float) {
-                return newValue(Type.FLOAT_TYPE);
+            	v = newValue(Type.FLOAT_TYPE);
+            	v.setExpressionValue(((Float) cst).toString(), ExpressionType.CONST);
+                return v;
             } else if (cst instanceof Long) {
-                return newValue(Type.LONG_TYPE);
+            	v = newValue(Type.LONG_TYPE);
+            	v.setExpressionValue(((Long) cst).toString(), ExpressionType.CONST);
+                return v;
             } else if (cst instanceof Double) {
-                return newValue(Type.DOUBLE_TYPE);
+            	v = newValue(Type.DOUBLE_TYPE);
+            	v.setExpressionValue(((Double) cst).toString(), ExpressionType.CONST);
+                return v;
             } else if (cst instanceof String) {
+            	// FIXME do we need strings?
                 return newValue(Type.getObjectType("java/lang/String"));
             } else if (cst instanceof Type) {
                 int sort = ((Type) cst).getSort();
@@ -83,8 +104,10 @@ public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
                         + cst);
             }
         case GETSTATIC:
+        	// TODO maybe rewrite instance of FieldInsnNode to map actual values of fields?
             return newValue(Type.getType(((FieldInsnNode) insn).desc));
         case NEW:
+        	// TODO here new "thread" object may be instantiated. is there something we need to do? 
             return newValue(Type.getObjectType(((TypeInsnNode) insn).desc));
         default:
             throw new Error("Internal error.");
@@ -94,15 +117,89 @@ public class ValInterpreter extends Interpreter<AnValue> implements Opcodes {
 	@Override
 	public AnValue copyOperation(AbstractInsnNode insn, AnValue value)
 			throws AnalyzerException {
-		// TODO Auto-generated method stub
-		return null;
+		return value.clone();
 	}
 
 	@Override
 	public AnValue unaryOperation(AbstractInsnNode insn, AnValue value)
 			throws AnalyzerException {
-		// TODO Auto-generated method stub
-		return null;
+		AnValue v = value.clone();
+		switch (insn.getOpcode()) {
+		case INEG:
+		case LNEG:
+		case DNEG:
+		case FNEG:
+			if (value.getExpType() != ExpressionType.UNDEFINED_EXP
+					&& value.getExpType() != ExpressionType.UNKNOWN) {
+				v.setInternalValue("-(" + value.getValue() + ")");
+				return v;
+			} else return value;
+		case IINC:
+			if (value.getExpType() != ExpressionType.UNDEFINED_EXP
+			&& value.getExpType() != ExpressionType.UNKNOWN) {
+				// NB value is integer, because otherwise the bytecode would be wrong.
+				v.setInternalValue("(" + value.getValue() + " + 1)");
+				return v;
+			} else return value;
+        case Opcodes.I2L:
+        case Opcodes.F2L:
+        case Opcodes.D2L:
+        	v.setClassName(AnValue.LONG_NAME);
+        	return v;
+        case Opcodes.I2F:
+        case Opcodes.L2F:
+        case Opcodes.D2F:
+        	v.setClassName(AnValue.FLOAT_NAME);
+        	return v;
+        case Opcodes.L2I:
+        case Opcodes.F2I:
+        case Opcodes.D2I:
+        	v.setClassName(AnValue.INT_NAME);
+        	return v;
+        case Opcodes.I2D:
+        case Opcodes.L2D:
+        case Opcodes.F2D:
+        	v.setClassName(AnValue.DOUBLE_NAME);
+        	return v;
+        case Opcodes.I2B:
+        	v.setClassName(AnValue.BOOL_NAME);
+        	return v;
+        case Opcodes.I2C:
+        	v.setClassName(AnValue.CHAR_NAME);
+        	return v;
+        case Opcodes.I2S:
+        	v.setClassName(AnValue.SHORT_NAME);
+        	return v;
+        case Opcodes.IFEQ:
+        case Opcodes.IFNE:
+        case Opcodes.IFLT:
+        case Opcodes.IFGE:
+        case Opcodes.IFGT:
+        case Opcodes.IFLE:
+        case Opcodes.IFNULL:
+        case Opcodes.IFNONNULL:
+        	// TODO jumps in interpreter. change the frame's behavioural types?
+        case Opcodes.IRETURN:
+        case Opcodes.LRETURN:
+        case Opcodes.FRETURN:
+        case Opcodes.DRETURN:
+        case Opcodes.ARETURN:
+        	// TODO behaviour types should be "0"
+        case Opcodes.TABLESWITCH:
+        case Opcodes.LOOKUPSWITCH:
+        case Opcodes.PUTSTATIC:
+        case Opcodes.GETFIELD:
+        case Opcodes.NEWARRAY:
+        case Opcodes.ANEWARRAY:
+        case Opcodes.ARRAYLENGTH:
+        case Opcodes.ATHROW:
+        case Opcodes.CHECKCAST:
+        case Opcodes.INSTANCEOF:
+        case Opcodes.MONITORENTER:
+        case Opcodes.MONITOREXIT:
+		default:
+            throw new Error("Internal error.");
+		}
 	}
 
 	@Override
--- src/com/laneve/asp/ASMAnalysis/asmTypes/AnValue.java
+++ src/com/laneve/asp/ASMAnalysis/asmTypes/AnValue.java
@@ -1,20 +1,19 @@
 package com.laneve.asp.ASMAnalysis.asmTypes;
 
-import org.objectweb.asm.tree.IincInsnNode;
-import org.objectweb.asm.tree.IntInsnNode;
-import org.objectweb.asm.tree.InvokeDynamicInsnNode;
-import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.TypeInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
-import org.objectweb.asm.tree.analysis.Value;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.analysis.Value;
 
 public class AnValue implements Value {
+	
+	public static String LONG_NAME = "LONG", SHORT_NAME = "SHORT",
+			INT_NAME = "INT", FLOAT_NAME = "FLOAT", DOUBLE_NAME = "DOUBLE",
+			BOOL_NAME = "BOOLEAN", CHAR_NAME = "CHAR", STRING_NAME = "STRING";
 	
 	public static String getClassName(Type t) {
 		switch(t.getSort()) {
 		case Type.ARRAY: return t.getInternalName() + "[]";
+		// TODO cambia in enum pubblici
 		case Type.BOOLEAN: return "BOOLEAN";
 		case Type.CHAR: return "CHAR";
 		case Type.DOUBLE: return "DOUBLE";
@@ -48,6 +47,43 @@ public class AnValue implements Value {
 		return getMajorSort(t1) == getMajorSort(t2);
 	}
 
+	public static String getConstValue(int opcode) {
+		switch(opcode) {
+		case Opcodes.ICONST_0:
+		case Opcodes.FCONST_0:
+		case Opcodes.DCONST_0:
+		case Opcodes.LCONST_0:
+			return "0";
+			
+		case Opcodes.ICONST_1:
+		case Opcodes.FCONST_1:
+		case Opcodes.DCONST_1:
+		case Opcodes.LCONST_1:
+			return "1";
+			
+		case Opcodes.ICONST_2:
+		case Opcodes.FCONST_2:
+			return "2";
+		
+		case Opcodes.ICONST_3:
+			return "3";
+		
+		case Opcodes.ICONST_4:
+			return "4";
+		
+		case Opcodes.ICONST_5:
+			return "5";
+
+		case Opcodes.ICONST_M1:
+			return "-1";
+
+		default:
+			return "";
+		}
+	}
+
+	
+	
 	public enum ExpressionType {CONST, VARIABLE, UNKNOWN, CONST_EXP, VAR_EXP, UNDEFINED_EXP};
 	protected static int maxID = 0;
 	protected static int generateID() {
@@ -58,7 +94,7 @@ public class AnValue implements Value {
 	protected String className;
 	protected String value;
 	protected ExpressionType exType;
-	protected int ID;	
+	protected int ID;
 	
 	
 	public AnValue(Type t) {
@@ -66,7 +102,16 @@ public class AnValue implements Value {
 		type = t;
 		className = getClassName(t);
 	}
-
+	
+	public AnValue(AnValue a) {
+		type = a.type;
+		className = a.className;
+		value = a.value;
+		exType = a.exType;
+		ID = a.ID;
+	}
+	
+/*
 	public AnValue(Type t, IincInsnNode n) {
 		ID = generateID();
 		type = t;
@@ -115,11 +160,43 @@ public class AnValue implements Value {
 		className = getClassName(t);
 		// TODO
 	}
-
+ */
 	public String getClassName() {
 		return className;
 	}
 
+	public String getValue() {
+		return value;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public ExpressionType getExpType() {
+		return exType;
+	}
+	
+	public AnValue clone() {
+		return new AnValue(this);
+	}
+	
+	public void setInternalValue(String val) {
+		value = val;
+	}
+	
+	public void setClassName(String name) {
+		className = name;
+	}
+	
+	public void setExpressionType(ExpressionType t) {
+		exType = t;
+	}
+	
+	public void setExpressionValue(String value, ExpressionType expression) {
+		this.value = value;
+		this.exType = expression;
+	}
 	
 	@Override
 	public int getSize() {
@@ -127,6 +204,8 @@ public class AnValue implements Value {
 			return 2;
 		return 1;
 	}
+
+	
 
 
 }
