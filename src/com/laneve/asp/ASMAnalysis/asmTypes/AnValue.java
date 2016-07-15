b699a0adaa0f3e5184671739a15da7ce815f3d66
fagiodarkie <fagio.darkie@gmail.com>
2016-07-15 15:42:50 +0200

    AnValue becomes the basic class. Subclasses are IntExpression, BoolExpression, Thread, Object, Undefined.

--- src/com/laneve/asp/ASMAnalysis/asmTypes/AnValue.java
+++ src/com/laneve/asp/ASMAnalysis/asmTypes/AnValue.java
@@ -1,15 +1,17 @@
 package com.laneve.asp.ASMAnalysis.asmTypes;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.analysis.Value;
 
-public class AnValue implements Value {
+public class AnValue extends Value {
 	
 	public static String LONG_NAME = "LONG", SHORT_NAME = "SHORT",
 			INT_NAME = "INT", FLOAT_NAME = "FLOAT", DOUBLE_NAME = "DOUBLE",
 			BOOL_NAME = "BOOLEAN", CHAR_NAME = "CHAR", STRING_NAME = "STRING",
-			REF_NAME = "REFERENCE";
+			REF_NAME = "REFERENCE", THREAD_NAME = "THREAD";
 	
 	public static String getClassName(Type t) {
 		switch(t.getSort()) {
@@ -22,8 +24,24 @@ public class AnValue implements Value {
 		case Type.LONG: return LONG_NAME;
 		case Type.SHORT: return SHORT_NAME;
 		case Type.METHOD: return t.getDescriptor();
-		case Type.OBJECT: return t.getInternalName();
+		case Type.OBJECT: {
+			if (t.getInternalName().endsWith(".Thread"))
+				return THREAD_NAME;
+			return t.getInternalName();
+		}
 		default: return "UNKNOWN";
+		}
+	}
+	
+	public static boolean typable(Type t) {
+		switch (t.getSort()) {
+		case Type.INT:
+		case Type.LONG:
+			return true;
+		case Type.OBJECT:
+			if (getClassName(t) == THREAD_NAME)
+				return true;
+		default: return false;
 		}
 	}
 	
@@ -47,69 +65,39 @@ public class AnValue implements Value {
 		return getMajorSort(t1) == getMajorSort(t2);
 	}
 
-	public static String getConstValue(int opcode) {
+	public static Long getConstValue(int opcode) {
 		switch(opcode) {
 		case Opcodes.ICONST_0:
-		case Opcodes.FCONST_0:
-		case Opcodes.DCONST_0:
 		case Opcodes.LCONST_0:
-			return "0";
+			return new Long(0);
 			
 		case Opcodes.ICONST_1:
-		case Opcodes.FCONST_1:
-		case Opcodes.DCONST_1:
 		case Opcodes.LCONST_1:
-			return "1";
+			return new Long(1);
 			
 		case Opcodes.ICONST_2:
-		case Opcodes.FCONST_2:
-			return "2";
+			return new Long(2);
 		
 		case Opcodes.ICONST_3:
-			return "3";
+			return new Long(3);
 		
 		case Opcodes.ICONST_4:
-			return "4";
+			return new Long(4);
 		
 		case Opcodes.ICONST_5:
-			return "5";
+			return new Long(5);
 
 		case Opcodes.ICONST_M1:
-			return "-1";
+			return new Long(-1);
 
 		default:
-			return "";
+			return null;
 		}
 	}
-
-	public static ExpressionType leastUpperBound(ExpressionType t1, ExpressionType t2) {
-		if (t1 == ExpressionType.UNKNOWN || t2 == ExpressionType.UNKNOWN)
-			return ExpressionType.UNKNOWN;
-		if (t1 == ExpressionType.UNDEFINED_EXP || t2 == ExpressionType.UNDEFINED_EXP)
-			return ExpressionType.UNDEFINED_EXP;
-		if (t1 == ExpressionType.VAR_EXP || t2 == ExpressionType.VAR_EXP)
-			return ExpressionType.VAR_EXP;
-		if (t1 == ExpressionType.VARIABLE || t2 == ExpressionType.VARIABLE)
-			return ExpressionType.VARIABLE;
-		if (t1 == ExpressionType.CONST_EXP || t2 == ExpressionType.CONST_EXP)
-			return ExpressionType.CONST_EXP;
-		return ExpressionType.CONST;
-
-		
-	}
 	
-	
-	public enum ExpressionType {CONST, VARIABLE, UNKNOWN, CONST_EXP, VAR_EXP, UNDEFINED_EXP};
-	protected static int maxID = 0;
-	protected static int generateID() {
-		return maxID++;
-	}
 	
 	protected Type type;
 	protected String className;
-	protected String value;
-	protected ExpressionType exType;
-	// protected int ID;
 	
 	
 	public AnValue(Type t) {
@@ -121,79 +109,18 @@ public class AnValue implements Value {
 	public AnValue(AnValue a) {
 		type = a.type;
 		className = a.className;
-		value = a.value;
-		exType = a.exType;
-		//ID = a.ID;
 	}
-	
-	public String toString() {
-		String type =(defined() ?  className : "-");
-		return getValue() + ":" + type;
-	}
-	
-	public boolean defined() {
-		return (exType != ExpressionType.UNDEFINED_EXP && exType != ExpressionType.UNKNOWN);
-	}
-	
-	public boolean equals(AnValue o) {
-		//if (ID != o.ID)
-		//	return false;
-		if (exType != o.exType)
-			return false;
-		if (className != o.className)
-			return false;
-		if (type != o.type)
-			return false;
-		if (value != o.value)
-			return false;
-		return true;
-	}
-	
+			
 	public String getClassName() {
 		return className;
 	}
 
-	public String getValue() {
-		return value;
-	}
-	
-//	public int getID() {
-//		return ID;
-//	}
-	
-	public ExpressionType getExpType() {
-		return exType;
-	}
-	
 	public AnValue clone() {
 		return new AnValue(this);
-	}
-	
-	public void setInternalValue(String val) {
-		value = val;
 	}
 	
 	public void setClassName(String name) {
 		className = name;
 	}
 	
-	public void setExpressionType(ExpressionType t) {
-		exType = t;
-	}
-	
-	public void setExpressionValue(String value, ExpressionType expression) {
-		this.value = value;
-		this.exType = expression;
-	}
-	
-	@Override
-	public int getSize() {
-		if (type.getSort() == Type.DOUBLE || type.getSort() == Type.LONG)
-			return 2;
-		return 1;
-	}
-
-	
-
-
 }
